78c2f6ced344f2acd71e21a9132f1d8d150819e9
mikhayiyl <duhnycan@gmail.com>
2022-02-20 17:40:54 +0300

    initial commit

---
+++ build/precache-manifest.30be8bbdbe3a6a5f08597a0d6e648ef0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71dde3020e7463805084a792a026df81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5edf7f92dac1e26f085a",
+    "url": "/static/css/2.b8803beb.chunk.css"
+  },
+  {
+    "revision": "5d3495182205516a75cb",
+    "url": "/static/css/main.fe154616.chunk.css"
+  },
+  {
+    "revision": "5edf7f92dac1e26f085a",
+    "url": "/static/js/2.fa75c8a7.chunk.js"
+  },
+  {
+    "revision": "9b8e5f68060b376b72c172b51d3acf5d",
+    "url": "/static/js/2.fa75c8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d3495182205516a75cb",
+    "url": "/static/js/main.77beea48.chunk.js"
+  },
+  {
+    "revision": "834bf5b7b6286fb14cdc",
+    "url": "/static/js/runtime-main.8158471b.js"
+  },
+  {
+    "revision": "4c1b3e65656e29db29e5e1253b017806",
+    "url": "/static/media/apps.4c1b3e65.jpg"
+  },
+  {
+    "revision": "1378c52e9049653a30163fb7bb1ea301",
+    "url": "/static/media/beach.1378c52e.jpg"
+  },
+  {
+    "revision": "6ae5a5e921debbaa5125c98236ffba63",
+    "url": "/static/media/data.6ae5a5e9.jpg"
+  },
+  {
+    "revision": "4ada99d8041f668c88ad212fa321dd6c",
+    "url": "/static/media/sun.4ada99d8.jpg"
+  },
+  {
+    "revision": "2b4df64a9da69c2fa8a29da81082713c",
+    "url": "/static/media/user.2b4df64a.jpg"
+  },
+  {
+    "revision": "c0c1e1f8d5411d8b70b4b91a19df098c",
+    "url": "/static/media/video.c0c1e1f8.mp4"
+  }
+]);
